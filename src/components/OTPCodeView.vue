a99f81d5718b26929418c74c7128071c5a8f719b
qazaleh <qazaleh.afrough@gmail.com>
2020-02-01 17:16:38 +0330

    create login dialog with error

---
+++ src/components/OTPCodeView.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "OTPCodeView"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/OTPLoginView.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "OTPLoginView"
+    }
+</script>
+
+<style scoped>
+
+</style>
